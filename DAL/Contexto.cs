eacbe6920d42560c1887ebc4499204f1d98d9595
Randy <randyfabianbelliard191407@gmail.com>
2023-10-02 19:37:01 -0400

    Aregando el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,8 +3,8 @@ using Microsoft.EntityFrameworkCore;
 namespace Randy_AP1_P1{
 
     public class Contexto: DbContext{
-
-      public Contexto(DbContextOptions<Contexto> options)
+      public DbSet<Aportes> Aportes { get; set; }
+      public Contexto(DbContextOptions<Contexto> options) : base (options)
       {
 
       }   
