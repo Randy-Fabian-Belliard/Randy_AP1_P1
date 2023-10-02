5bdbf78047d2331b9e6ba756b1decf72d44cc12b
Randy <randyfabianbelliard191407@gmail.com>
2023-10-02 19:39:45 -0400

    Agregando la migracion

---
+++ Migrations/20231002232716_Segunda.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Randy_AP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Segunda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Conteo",
+                table: "Aportes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Conteo",
+                table: "Aportes");
+        }
+    }
+}
